5cb5b6bda2bfdc73b07bb8c0946e055f45621dd8
Marek Ciążyński <m.ciazynski@bfirst.tech>
2018-10-15 23:43:42 +0200

    Game version 0.4

--- kveiki.py
+++ kveiki.py
@@ -5,7 +5,6 @@ import curses
 import time
 import atexit
 
-
 @atexit.register
 def close_curses():
 	curses.nocbreak()
@@ -70,12 +69,16 @@ def close_curses():
 		print '\033[0;0;0m'
 
 
+
 class player:
 	def __init__(self):
+		self.solid = ['X','R','G','B','Y','D']
+		self.movable = ['C','K','r','g','b','y',' ']
+		
 		self.coins = 0
 		self.coinsMAX = 0
-		self.finish = False
 		self.restoreF = False
+		self.finish = False
 
 		for i in lvl_structure:
 			try:
@@ -93,58 +96,113 @@ class player:
 		#stdscr.addstr(0,0,str(self.x)+' '+str(self.y))
 
 	def goUP(self):
-		if lvl_structure[self.y-1][self.x] != 'X':
-			if lvl_structure[self.y-1][self.x] != 'D':
-				if lvl_structure[self.y-1][self.x] != 'R':
-					if lvl_structure[self.y-1][self.x] != 'G':
-						if lvl_structure[self.y-1][self.x] != 'B':
-							if lvl_structure[self.y-1][self.x] != 'Y':
-								self.xL = self.x
-								self.yL = self.y
+		if not lvl_structure[self.y-1][self.x] in self.solid:
+			if lvl_structure[self.y-1][self.x] == 'Z':
+				if (lvl_structure[self.y-2][self.x] in self.movable) and (lvl_structure[self.y-3][self.x] == ' ' or lvl_structure[self.y-2][self.x] == ' '):
+					if lvl_structure[self.y-3][self.x] == ' ': set_char_in_structure(self.y-3,self.x,lvl_structure[self.y-2][self.x])
+					set_char_in_structure(self.y-2,self.x,lvl_structure[self.y-1][self.x])
+					set_char_in_structure(self.y-1,self.x,' ')
+					for i in xrange(1,4):
+						if lvl_structure[self.y-i][self.x] == ' ': stdscr.addstr(self.y+3-i,self.x,' ',curses.color_pair(1))
+						elif lvl_structure[self.y-i][self.x] == 'C': stdscr.addstr(self.y+3-i,self.x,'$',curses.color_pair(4))
+						elif lvl_structure[self.y-i][self.x] == 'K': stdscr.addstr(self.y+3-i,self.x,'F',curses.color_pair(1))
+						elif lvl_structure[self.y-i][self.x] == 'r': stdscr.addstr(self.y+3-i,self.x,'F',curses.color_pair(7))
+						elif lvl_structure[self.y-i][self.x] == 'g': stdscr.addstr(self.y+3-i,self.x,'F',curses.color_pair(8))
+						elif lvl_structure[self.y-i][self.x] == 'b': stdscr.addstr(self.y+3-i,self.x,'F',curses.color_pair(3))
+						elif lvl_structure[self.y-i][self.x] == 'y': stdscr.addstr(self.y+3-i,self.x,'F',curses.color_pair(4))
+						elif lvl_structure[self.y-i][self.x] == 'Z': stdscr.addstr(self.y+3-i,self.x,'X',curses.color_pair(9))
 
-								self.y-=1
-								self.update()
+					self.xL = self.x
+					self.yL = self.y
+					self.y-=1
+					self.update()
+			else:
+				self.xL = self.x
+				self.yL = self.y
+				self.y-=1
+				self.update()
 
 	def goDOWN(self):
-		#print lvl_structure[self.y+1][self.x] != 'X'
-		if lvl_structure[self.y+1][self.x] != 'X':
-			if lvl_structure[self.y+1][self.x] != 'D':
-				if lvl_structure[self.y+1][self.x] != 'R':
-					if lvl_structure[self.y+1][self.x] != 'G':
-						if lvl_structure[self.y+1][self.x] != 'B':
-							if lvl_structure[self.y+1][self.x] != 'Y':
-								self.xL = self.x
-								self.yL = self.y
+		if not lvl_structure[self.y+1][self.x] in self.solid:
+			if lvl_structure[self.y+1][self.x] == 'Z':
+				if (lvl_structure[self.y+2][self.x] in self.movable) and (lvl_structure[self.y+3][self.x] == ' ' or lvl_structure[self.y+2][self.x] == ' '):
+					if lvl_structure[self.y+3][self.x] == ' ': set_char_in_structure(self.y+3,self.x,lvl_structure[self.y+2][self.x])
+					set_char_in_structure(self.y+2,self.x,lvl_structure[self.y+1][self.x])
+					set_char_in_structure(self.y+1,self.x,' ')
+					for i in xrange(1,4):
+						if lvl_structure[self.y+i][self.x] == ' ': stdscr.addstr(self.y+3+i,self.x,' ',curses.color_pair(1))
+						elif lvl_structure[self.y+i][self.x] == 'C': stdscr.addstr(self.y+3+i,self.x,'$',curses.color_pair(4))
+						elif lvl_structure[self.y+i][self.x] == 'K': stdscr.addstr(self.y+3+i,self.x,'F',curses.color_pair(1))
+						elif lvl_structure[self.y+i][self.x] == 'r': stdscr.addstr(self.y+3+i,self.x,'F',curses.color_pair(7))
+						elif lvl_structure[self.y+i][self.x] == 'g': stdscr.addstr(self.y+3+i,self.x,'F',curses.color_pair(8))
+						elif lvl_structure[self.y+i][self.x] == 'b': stdscr.addstr(self.y+3+i,self.x,'F',curses.color_pair(3))
+						elif lvl_structure[self.y+i][self.x] == 'y': stdscr.addstr(self.y+3+i,self.x,'F',curses.color_pair(4))
+						elif lvl_structure[self.y+i][self.x] == 'Z': stdscr.addstr(self.y+3+i,self.x,'X',curses.color_pair(9))
 
-								self.y+=1
-								self.update()
+					self.xL = self.x
+					self.yL = self.y
+					self.y+=1
+					self.update()
+			else:
+				self.xL = self.x
+				self.yL = self.y
+				self.y+=1
+				self.update()
 
 	def goLEFT(self):
-		if lvl_structure[self.y][self.x-1] != 'X':
-			if lvl_structure[self.y][self.x-1] != 'D':
-				if lvl_structure[self.y][self.x-1] != 'R':
-					if lvl_structure[self.y][self.x-1] != 'G':
-						if lvl_structure[self.y][self.x-1] != 'B':
-							if lvl_structure[self.y][self.x-1] != 'Y':
-								self.xL = self.x
-								self.yL = self.y
+		if not lvl_structure[self.y][self.x-1] in self.solid:
+			if lvl_structure[self.y][self.x-1] == 'Z':
+				if (lvl_structure[self.y][self.x-2] in self.movable) and (lvl_structure[self.y][self.x-3] == ' ' or lvl_structure[self.y][self.x-2] == ' '):
+					if lvl_structure[self.y][self.x-3] == ' ': set_char_in_structure(self.y,self.x-3,lvl_structure[self.y][self.x-2])
+					set_char_in_structure(self.y,self.x-2,lvl_structure[self.y][self.x-1])
+					set_char_in_structure(self.y,self.x-1,' ')
+					for i in xrange(1,4):
+						if lvl_structure[self.y][self.x-i] == ' ': stdscr.addstr(self.y+3,self.x-i,' ',curses.color_pair(1))
+						elif lvl_structure[self.y][self.x-i] == 'C': stdscr.addstr(self.y+3,self.x-i,'$',curses.color_pair(4))
+						elif lvl_structure[self.y][self.x-i] == 'K': stdscr.addstr(self.y+3,self.x-i,'F',curses.color_pair(1))
+						elif lvl_structure[self.y][self.x-i] == 'r': stdscr.addstr(self.y+3,self.x-i,'F',curses.color_pair(7))
+						elif lvl_structure[self.y][self.x-i] == 'g': stdscr.addstr(self.y+3,self.x-i,'F',curses.color_pair(8))
+						elif lvl_structure[self.y][self.x-i] == 'b': stdscr.addstr(self.y+3,self.x-i,'F',curses.color_pair(3))
+						elif lvl_structure[self.y][self.x-i] == 'y': stdscr.addstr(self.y+3,self.x-i,'F',curses.color_pair(4))
+						elif lvl_structure[self.y][self.x-i] == 'Z': stdscr.addstr(self.y+3,self.x-i,'X',curses.color_pair(9))
 
-								self.x-=1
-								self.update()
+					self.xL = self.x
+					self.yL = self.y
+					self.x-=1
+					self.update()
+			else:
+				self.xL = self.x
+				self.yL = self.y
+				self.x-=1
+				self.update()
 
 
 	def goRIGHT(self):
-		if lvl_structure[self.y][self.x+1] != 'X':
-			if lvl_structure[self.y][self.x+1] != 'D':
-				if lvl_structure[self.y][self.x+1] != 'R':
-					if lvl_structure[self.y][self.x+1] != 'G':
-						if lvl_structure[self.y][self.x+1] != 'B':
-							if lvl_structure[self.y][self.x+1] != 'Y':
-								self.xL = self.x
-								self.yL = self.y
+		if not lvl_structure[self.y][self.x+1] in self.solid:
+			if lvl_structure[self.y][self.x+1] == 'Z':
+				if (lvl_structure[self.y][self.x+2] in self.movable) and (lvl_structure[self.y][self.x+3] == ' ' or lvl_structure[self.y][self.x+2] == ' '):
+					if lvl_structure[self.y][self.x+3] == ' ': set_char_in_structure(self.y,self.x+3,lvl_structure[self.y][self.x+2])
+					set_char_in_structure(self.y,self.x+2,lvl_structure[self.y][self.x+1])
+					set_char_in_structure(self.y,self.x+1,' ')
+					for i in xrange(1,4):
+						if lvl_structure[self.y][self.x+i] == ' ': stdscr.addstr(self.y+3,self.x+i,' ',curses.color_pair(1))
+						elif lvl_structure[self.y][self.x+i] == 'C': stdscr.addstr(self.y+3,self.x+i,'$',curses.color_pair(4))
+						elif lvl_structure[self.y][self.x+i] == 'K': stdscr.addstr(self.y+3,self.x+i,'F',curses.color_pair(1))
+						elif lvl_structure[self.y][self.x+i] == 'r': stdscr.addstr(self.y+3,self.x+i,'F',curses.color_pair(7))
+						elif lvl_structure[self.y][self.x+i] == 'g': stdscr.addstr(self.y+3,self.x+i,'F',curses.color_pair(8))
+						elif lvl_structure[self.y][self.x+i] == 'b': stdscr.addstr(self.y+3,self.x+i,'F',curses.color_pair(3))
+						elif lvl_structure[self.y][self.x+i] == 'y': stdscr.addstr(self.y+3,self.x+i,'F',curses.color_pair(4))
+						elif lvl_structure[self.y][self.x+i] == 'Z': stdscr.addstr(self.y+3,self.x+i,'X',curses.color_pair(9))
 
-								self.x+=1
-								self.update()
+					self.xL = self.x
+					self.yL = self.y
+					self.x+=1
+					self.update()
+			else:
+				self.xL = self.x
+				self.yL = self.y
+				self.x+=1
+				self.update()
 
 
 	def update(self):
@@ -160,14 +218,14 @@ class player:
 
 		if lvl_structure[self.y][self.x] == 'F':
 			if self.coins == self.coinsMAX:
-				stdscr.addstr(0,0,'Done')
+#				stdscr.addstr(0,0,'Done')
 				stdscr.refresh()
 				self.finish = True
 			else:
 				self.restoreF = True
 
 		if lvl_structure[self.y][self.x] == 'C':
-			clear_char_in_structure(self.y,self.x)
+			set_char_in_structure(self.y,self.x,' ')
 			self.coins += 1
 
 		if lvl_structure[self.y][self.x] == 'K': find ='D'
@@ -183,13 +241,13 @@ class player:
 					if j == find:
 						self.tmpx=i.index(find)
 						self.tmpy=lvl_structure.index(i)
-						clear_char_in_structure(self.tmpy,self.tmpx)
+						set_char_in_structure(self.tmpy,self.tmpx,' ')
 						stdscr.addstr(self.tmpy+3,self.tmpx,' ', curses.color_pair(1) )
 						stdscr.refresh()
 
 
-def clear_char_in_structure(y,x):
-	lvl_structure[y][x] = ' '
+def set_char_in_structure(y,x,char):
+	lvl_structure[y][x] = char
 
 
 dirs_with_lvl = []
@@ -228,6 +286,105 @@ stdscr.keypad(1)
 curses.start_color()
 curses.curs_set(0)
 
+def main_game():
+	global sec
+	global minutes
+	global lvl_structure
+	#Wczytanie pliku level'u
+	lvl_structure=[]
+	tmp = []
+	for line in xrange(1,len(open(file_name, 'rU').readlines())+1):
+		tmp = []
+		for char in linecache.getline(file_name, line):
+			if char != '\n':
+				tmp.append(char)
+		lvl_structure.append(tmp)
+	gracz = player()
+	
+	stdscr.addstr(3,0,'')
+	for char in open(file_name).read():
+		if char == 'X':
+			stdscr.addstr( "#", curses.color_pair(1) )
+		elif char == ' ':
+			stdscr.addstr( " ", curses.color_pair(1) )
+		elif char == 's':
+			stdscr.addstr( " ", curses.color_pair(2) )
+		elif char == 'P':
+			stdscr.addstr( "@", curses.color_pair(2) )
+		elif char == 'F':
+			stdscr.addstr( "^", curses.color_pair(3) )
+		elif char == 'C':
+			stdscr.addstr( "$", curses.color_pair(4) )
+		elif char == 'D':
+			stdscr.addstr( "=", curses.color_pair(1) )
+		elif char == 'K':
+			stdscr.addstr( "F", curses.color_pair(1) )
+		elif char == 'G':
+			stdscr.addstr( "=", curses.color_pair(8) )
+		elif char == 'g':
+			stdscr.addstr( "F", curses.color_pair(8) )
+		elif char == 'R':
+			stdscr.addstr( "=", curses.color_pair(7) )
+		elif char == 'r':
+			stdscr.addstr( "F", curses.color_pair(7) )
+		elif char == 'Y':
+			stdscr.addstr( "=", curses.color_pair(4) )
+		elif char == 'y':
+			stdscr.addstr( "F", curses.color_pair(4) )
+		elif char == 'B':
+			stdscr.addstr( "=", curses.color_pair(3) )
+		elif char == 'b':
+			stdscr.addstr( "F", curses.color_pair(3) )
+		elif char == 'Z':
+			stdscr.addstr( "X", curses.color_pair(9) )
+		elif char == '\n':
+			stdscr.addstr( "\n", curses.color_pair(1) )
+			line+=1
+		stdscr.refresh()
+
+	while gracz.finish == False:
+		winlin, wincol =  stdscr.getmaxyx()
+		stdscr.addstr(winlin-2,0,'Monety:', curses.color_pair(6) )
+		stdscr.attron(curses.A_BOLD)
+		stdscr.addstr(winlin-2,9,str(gracz.coins)+'/'+str(gracz.coinsMAX)+' ', curses.color_pair(10) )
+
+		now_time=time.time()
+		now_time=int(now_time)
+		sec=now_time-start_time
+		minutes=sec/60
+		##stdscr.addstr(20,0,str(sec))
+		##stdscr.addstr(20,5,str(sec%60))
+		sec=sec%60
+		sec=str(sec)
+		minutes=str(minutes)
+		if len(sec)==1: sep='0'
+		else: sep=''
+		stdscr.addstr(winlin-2,16,'Czas:', curses.color_pair(6) )
+		stdscr.attron(curses.A_BOLD)
+		stdscr.addstr(winlin-2,24,minutes+':'+sep+sec, curses.color_pair(10) )
+		if (lvl_structure[gracz.y][gracz.x-1]=='Z' and lvl_structure[gracz.y][gracz.x-2] == 'C' and (lvl_structure[gracz.y-1][gracz.x-1] in gracz.solid or lvl_structure[gracz.y+1][gracz.x-1] in gracz.solid) and  lvl_structure[gracz.y-1][gracz.x-2] in gracz.solid and lvl_structure[gracz.y+1][gracz.x-2] in gracz.solid and lvl_structure[gracz.y][gracz.x-3] in gracz.solid) or (lvl_structure[gracz.y][gracz.x+1]=='Z' and lvl_structure[gracz.y][gracz.x+2] == 'C' and (lvl_structure[gracz.y-1][gracz.x+1] in gracz.solid or lvl_structure[gracz.y+1][gracz.x+1] in gracz.solid) and  lvl_structure[gracz.y-1][gracz.x+2] in gracz.solid and lvl_structure[gracz.y+1][gracz.x+2] in gracz.solid and lvl_structure[gracz.y][gracz.x+3] in gracz.solid) or (lvl_structure[gracz.y-1][gracz.x]=='Z' and lvl_structure[gracz.y-2][gracz.x] == 'C' and (lvl_structure[gracz.y-1][gracz.x-1] in gracz.solid or lvl_structure[gracz.y-1][gracz.x+1] in gracz.solid) and  lvl_structure[gracz.y-2][gracz.x-1] in gracz.solid and lvl_structure[gracz.y-2][gracz.x+1] in gracz.solid and lvl_structure[gracz.y-3][gracz.x] in gracz.solid) or (lvl_structure[gracz.y+1][gracz.x]=='Z' and lvl_structure[gracz.y+2][gracz.x] == 'C' and (lvl_structure[gracz.y+1][gracz.x-1] in gracz.solid or lvl_structure[gracz.y+1][gracz.x+1] in gracz.solid) and lvl_structure[gracz.y+2][gracz.x-1] in gracz.solid and lvl_structure[gracz.y+2][gracz.x+1] in gracz.solid and lvl_structure[gracz.y+3][gracz.x] in gracz.solid):
+			stdscr.addstr(winlin-2,39,'R - restart',curses.A_BLINK+curses.A_BOLD)
+		else:
+			stdscr.addstr(winlin-2,39,'R - restart',curses.A_NORMAL+curses.A_BOLD)
+		
+		stdscr.refresh()
+		for i in xrange(0,wincol-1):
+			stdscr.addstr(winlin-1,i,' ')
+			stdscr.addstr(winlin-3,i,' ')
+			stdscr.addstr(winlin-4,i,' ')
+			stdscr.addstr(winlin-5,i,' ')
+
+		key = stdscr.getch()
+		if key == curses.KEY_UP or key == ord('w'):	gracz.goUP()
+		elif key == curses.KEY_DOWN or key == ord('s'): gracz.goDOWN()
+		elif key == curses.KEY_LEFT or key == ord('a'): gracz.goLEFT()
+		elif key == curses.KEY_RIGHT or key == ord('d'): gracz.goRIGHT()
+		elif key == ord('r'):
+			main_game()
+			break
+		elif key == ord('q'): exit()
+
+
 winlin, wincol =  stdscr.getmaxyx()
 curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
 curses.init_pair(2,curses.COLOR_BLACK,curses.COLOR_WHITE)
@@ -238,10 +395,7 @@ if os.path.exists(os.path.join(dirs_with_lvl[choose-1],'discribe')):
 	stdscr.addstr(winlin-2,1,'\n<ENTER>', curses.color_pair(2) )
 	stdscr.getch()
 
-if sys.argv != 2:
-	chosen_directory = os.listdir(dirs_with_lvl[choose-1])
-else:
-	chosen_directory = os.listdir(os.path.join('.',sys.argv[1]))
+chosen_directory = os.listdir(dirs_with_lvl[choose-1])
 times=[]
 for level in sorted(chosen_directory):
 	if level.endswith('.lvl'):
@@ -258,7 +412,7 @@ for level in sorted(chosen_directory):
 		stdscr.addstr(0,0,'\nLevel'+' '+str(sorted(os.listdir(dirs_with_lvl[choose-1])).index(level)+1)+' ('+level+')\n\n', curses.color_pair(5))
 
 		file_name = os.path.join(dirs_with_lvl[choose-1],level)
-		lvlfile = open(file_name)
+		#lvlfile = open(file_name)
 		#Przypisanie kolorów
 		curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE) # ściana, drzwi, klucz
 		curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK) #gracz
@@ -268,83 +422,7 @@ for level in sorted(chosen_directory):
 		curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_YELLOW) # pasek na dole ekranu
 		curses.init_pair(7, curses.COLOR_RED, curses.COLOR_WHITE) # czerwony klucz i drzwi
 		curses.init_pair(8, curses.COLOR_GREEN, curses.COLOR_WHITE) # zielony klucz i drzwi
-		#Wczytanie pliku level'u
-		lvl_structure=[]
-		tmp = []
-		for line in xrange(1,len(open(file_name, 'rU').readlines())+1):
-			tmp = []
-			for char in linecache.getline(file_name, line):
-				if char != '\n':
-					tmp.append(char)
-			lvl_structure.append(tmp)
-		gracz = player()
-		for char in lvlfile.read():
-			if char == 'X':
-				stdscr.addstr( "#", curses.color_pair(1) )
-			elif char == ' ':
-				stdscr.addstr( " ", curses.color_pair(1) )
-			elif char == 's':
-				stdscr.addstr( " ", curses.color_pair(2) )
-			elif char == 'P':
-				stdscr.addstr( "@", curses.color_pair(2) )
-			elif char == 'F':
-				stdscr.addstr( "^", curses.color_pair(3) )
-			elif char == 'C':
-				stdscr.addstr( "$", curses.color_pair(4) )
-			elif char == 'D':
-				stdscr.addstr( "=", curses.color_pair(1) )
-			elif char == 'K':
-				stdscr.addstr( "F", curses.color_pair(1) )
-			elif char == 'G':
-				stdscr.addstr( "=", curses.color_pair(8) )
-			elif char == 'g':
-				stdscr.addstr( "F", curses.color_pair(8) )
-			elif char == 'R':
-				stdscr.addstr( "=", curses.color_pair(7) )
-			elif char == 'r':
-				stdscr.addstr( "F", curses.color_pair(7) )
-			elif char == 'Y':
-				stdscr.addstr( "=", curses.color_pair(4) )
-			elif char == 'y':
-				stdscr.addstr( "F", curses.color_pair(4) )
-			elif char == 'B':
-				stdscr.addstr( "=", curses.color_pair(3) )
-			elif char == 'b':
-				stdscr.addstr( "F", curses.color_pair(3) )
-			elif char == '\n':
-				stdscr.addstr( "\n", curses.color_pair(1) )
-				line+=1
-			stdscr.refresh()
-
-		while gracz.finish == False:
-			winlin, wincol =  stdscr.getmaxyx()
-			stdscr.addstr(winlin-2,0,'Monety:', curses.color_pair(6) )
-			stdscr.attron(curses.A_BOLD)
-			stdscr.addstr(winlin-2,9,str(gracz.coins)+'/'+str(gracz.coinsMAX))
-
-			now_time=time.time()
-			now_time=int(now_time)
-			sec=now_time-start_time
-			minutes=sec/60
-			##stdscr.addstr(20,0,str(sec))
-			##stdscr.addstr(20,5,str(sec%60))
-			sec=sec%60
-			sec=str(sec)
-			minutes=str(minutes)
-			if len(sec)==1: sep='0'
-			else: sep=''
-			stdscr.addstr(winlin-2,16,'Czas:', curses.color_pair(6) )
-			stdscr.attron(curses.A_BOLD)
-			stdscr.addstr(winlin-2,24,minutes+':'+sep+sec)
-			for i in xrange(0,wincol-1):
-				stdscr.addstr(winlin-1,i,' ')
-				stdscr.addstr(winlin-3,i,' ')
-				stdscr.addstr(winlin-4,i,' ')
-				stdscr.addstr(winlin-5,i,' ')
-
-			key = stdscr.getch()
-			if key == curses.KEY_UP or key == ord('w'):	gracz.goUP()
-			elif key == curses.KEY_DOWN or key == ord('s'): gracz.goDOWN()
-			elif key == curses.KEY_LEFT or key == ord('a'): gracz.goLEFT()
-			elif key == curses.KEY_RIGHT or key == ord('d'): gracz.goRIGHT()
-			elif key == ord('q'): exit()
+		curses.init_pair(9, curses.COLOR_WHITE, curses.COLOR_BLUE) # skrzynki
+		curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_BLACK) # kuflerz i kuptuż
+		main_game()
+		
